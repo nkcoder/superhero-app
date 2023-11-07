df36bb5955d844480f2198d3c196811b664f378d
Daniel <daniel5hbs@gmail.com>
2023-11-08 00:06:43 +1100

    fix the lambda to save user super hero

--- amplify/backend/function/superheroremoteapi/src/index.js
+++ amplify/backend/function/superheroremoteapi/src/index.js
@@ -1,7 +1,7 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = async event => {
+export const handler = async event => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
   const baseURL = "https://superheroapi.com/api/122136422408017955/search";
   console.log(`finding super hero by name: ${event.name}`);
--- amplify/backend/function/superherosave/src/index.js
+++ amplify/backend/function/superherosave/src/index.js
@@ -5,16 +5,22 @@ const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
 exports.handler = async event => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
+  console.log(`event received: ${JSON.stringify(event)}`);
+
+  const { userId, updateSuperHeroReq } = { ...event.arguments };
+  const { id, image, name, powerstats } = { ...updateSuperHeroReq };
   const command = new PutCommand({
     TableName: "superherodb-dev",
     Item: {
-      id: { N: "1" },
-      name: { S: "A-Bomb" },
+      id: Number(id),
+      name: name,
+      powerstats: powerstats,
+      image: image,
+      userId: userId,
     },
   });
 
   const response = await docClient.send(command);
-  console.log(response);
+  console.log(`put item response: ${response}`);
   return response;
 };
