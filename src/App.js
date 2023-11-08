faa93102db109fb2f48fac7b49626708e9e2219f
Daniel <daniel5hbs@gmail.com>
2023-11-08 11:55:07 +1100

    save super hero api is working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import { Amplify, API, graphqlOperation } from "aws-amplify";
 import { searchSuperheroes } from "./graphql/queries.js";
+import { savedSuperhero } from "./graphql/mutations.js";
 
 import awsExports from "./aws-exports.js";
 Amplify.configure(awsExports);
@@ -11,6 +12,8 @@ function App() {
 
   const [searchKey, setSearchKey] = useState("");
   const [superHeros, setSuperHeros] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [selectedHero, setSelectedHero] = useState({});
 
   const searchSuperHeros = async () => {
     console.log(`start to search super heros by :${searchKey}`);
@@ -23,10 +26,169 @@ function App() {
     }
   };
 
+  const saveSuperHeros = async () => {
+    console.log(`start to save your super hero: ${JSON.stringify(selectedHero)}`);
+    const updateSuperHeroReq = {
+      id: selectedHero.id,
+      name: selectedHero.name,
+      image: {
+        url: selectedHero.image.url,
+      },
+      powerstats: {
+        combat: selectedHero.powerstats.combat,
+        durability: selectedHero.powerstats.durability,
+        intelligence: selectedHero.powerstats.intelligence,
+        power: selectedHero.powerstats.power,
+        speed: selectedHero.powerstats.speed,
+        strength: selectedHero.powerstats.strength,
+      },
+    };
+    try {
+      console.log(`start to save your super hero: ${JSON.stringify(updateSuperHeroReq)}`);
+      await API.graphql(
+        graphqlOperation(savedSuperhero, {
+          userId: 1,
+          updateSuperHeroReq: updateSuperHeroReq,
+        })
+      );
+    } catch (err) {
+      console.log("error saving your super heros", err);
+    }
+  };
+
+  const toggleEditMode = superHero => {
+    setSelectedHero(superHero);
+    console.log(`editMode: ${editMode}`);
+
+    if (editMode) {
+      saveSuperHeros(selectedHero);
+    }
+
+    setEditMode(!editMode);
+  };
+
+  const Search = ({ searchKey, searchSuperHeros }) => {
+    return (
+      <div className="search">
+        <input
+          value={searchKey}
+          type="text"
+          placeholder="Search super heros"
+          minLength="2"
+          maxLength="30"
+          onChange={event => setSearchKey(event.target.value)}
+        />
+        <button onClick={searchSuperHeros}>Search</button>
+      </div>
+    );
+  };
+
+  const SuperHeros = ({ superheros }) => {
+    if (superheros.length === 0) {
+      return <p>No super heros found, please try something else.</p>;
+    }
+
+    const superHeros = superheros.map(hero => <SuperHero key={hero.id} superhero={hero} />);
+
+    return <ul className="superheros">{superHeros}</ul>;
+  };
+
+  const updatePowerStat = updatedData => {
+    setSelectedHero({
+      ...selectedHero,
+      ...updatedData,
+    });
+  };
+
+  const SuperHero = ({ superhero }) => {
+    return (
+      <li className="superhero">
+        <img src={superhero.image.url} alt="SuperHero" />
+        <div>
+          <h3>{superhero.name}</h3>
+          {!editMode ? (
+            <ShowPowerStats powerstats={superhero.powerstats} />
+          ) : (
+            <EditPowerStats powerstats={selectedHero.powerstats} updatePowerStat={updatePowerStat} />
+          )}
+          <button onClick={e => toggleEditMode(superhero)}>{editMode ? "Save" : "Edit"}</button>
+        </div>
+      </li>
+    );
+  };
+
+  const ShowPowerStats = ({ powerstats }) => {
+    return (
+      <div className="powerstats">
+        <li>combat: {powerstats.combat}</li>
+        <li>durability: {powerstats.durability}</li>
+        <li>intelligence: {powerstats.intelligence}</li>
+        <li>power: {powerstats.power}</li>
+        <li>speed: {powerstats.speed}</li>
+        <li>strength: {powerstats.strength}</li>
+      </div>
+    );
+  };
+
+  const EditPowerStats = ({ powerstats, updatePowerStat }) => {
+    return (
+      <div className="powerstats">
+        <span>
+          combat:{" "}
+          <input
+            type="text"
+            value={powerstats.combat}
+            onChange={event => updatePowerStat({ combat: event.target.value })}
+          />
+        </span>
+        <span>
+          durability:{" "}
+          <input
+            type="text"
+            value={powerstats.durability}
+            onChange={event => updatePowerStat({ durability: event.target.value })}
+          />
+        </span>
+        <span>
+          intelligence:{" "}
+          <input
+            type="text"
+            value={powerstats.intelligence}
+            onChange={event => updatePowerStat({ intelligence: event.target.value })}
+          />
+        </span>
+        <span>
+          power:{" "}
+          <input
+            type="text"
+            value={powerstats.power}
+            onChange={event => updatePowerStat({ power: event.target.value })}
+          />
+        </span>
+        <span>
+          speed:{" "}
+          <input
+            type="text"
+            value={powerstats.speed}
+            onChange={event => updatePowerStat({ speed: event.target.value })}
+          />
+        </span>
+        <span>
+          strength:{" "}
+          <input
+            type="text"
+            value={powerstats.strength}
+            onChange={event => updatePowerStat({ strength: event.target.value })}
+          />
+        </span>
+      </div>
+    );
+  };
+
   return (
     <div className="main">
       <Header />
-      <Search searchKey={searchKey} searchSuperHeros={searchSuperHeros} updateSearchKey={setSearchKey} />
+      <Search searchKey={searchKey} searchSuperHeros={searchSuperHeros} />
       <SuperHeros superheros={superHeros} />
     </div>
   );
@@ -37,51 +199,6 @@ const Header = () => {
     <header className="header">
       <h1>Super Heros</h1>
     </header>
-  );
-};
-
-const Search = ({ searchKey, searchSuperHeros, updateSearchKey }) => {
-  return (
-    <div className="search">
-      <input
-        value={searchKey}
-        type="text"
-        placeholder="Search super heros"
-        minLength="2"
-        maxLength="30"
-        onChange={event => updateSearchKey(event.target.value)}
-      />
-      <button onClick={searchSuperHeros}>Search</button>
-    </div>
-  );
-};
-
-const SuperHeros = ({ superheros }) => {
-  if (superheros.length === 0) {
-    return <p>No super heros found, please try something else.</p>;
-  }
-
-  const superHeros = superheros.map(hero => <SuperHero key={hero.id} superhero={hero} />);
-
-  return <ul className="superheros">{superHeros}</ul>;
-};
-
-const SuperHero = ({ superhero }) => {
-  return (
-    <li className="superhero">
-      <img src={superhero.image.url} alt="SuperHero" />
-      <div>
-        <h3>{superhero.name}</h3>
-        <div className="powerstats">
-          <span>combat: {superhero.powerstats.combat}</span>
-          <span>durability: {superhero.powerstats.durability}</span>
-          <span>intelligence: {superhero.powerstats.intelligence}</span>
-          <span>power: {superhero.powerstats.power}</span>
-          <span>speed: {superhero.powerstats.speed}</span>
-          <span>strength: {superhero.powerstats.strength}</span>
-        </div>
-      </div>
-    </li>
   );
 };
 
